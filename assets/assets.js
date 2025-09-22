3e6ea259fa53eb303c0570751c5e96068dedaa91
Maaz Ansari <maazansari25667@gmail.com>
2025-09-22 06:08:46 +0530

    chore(assets): branded tiles and icon paths

---
+++ assets/assets.js
@@ -0,0 +1,60 @@
+// assets/assets.js
+// Updated to use the new JPG brand images stored in /public
+
+import arrow_icon from "./arrow_icon.svg";
+import arrow_icon_dull from "./arrow_icon_dull.svg";
+import menu_icon from "./menu_icon.svg";
+import search_icon from "./search_icon.svg";
+import profile_icon from "./profile_icon.svg";
+import copy_icon from "./copy_icon.svg";
+import deepthink_icon from "./deepthink_icon.svg";
+import chat_icon from "./chat_icon.svg";
+import dislike_icon from "./dislike_icon.svg";
+import like_icon from "./like_icon.svg";
+import phone_icon from "./phone_icon.svg";
+import phone_icon_dull from "./phone_icon_dull.svg";
+import pencil_icon from "./pencil_icon.svg";
+import delete_icon from "./delete_icon.svg";
+import pin_icon from "./pin_icon.svg";
+import regenerate_icon from "./regenerate_icon.svg";
+import sidebar_icon from "./sidebar_icon.svg";
+import sidebar_close_icon from "./sidebar_close_icon.svg";
+import chat_icon_dull from "./chat_icon_dull.svg";
+import qrcode from "./qrcode.png";
+import three_dots from "./three_dots.svg";
+import new_icon from "./new_icon.svg";
+
+// NOTE: logo_icon now points to the new JPG in /public,
+// so we reference it by string path instead of importing an SVG.
+export const assets = {
+  // brand
+  logo_icon: "/next.jpg",        // main PromptPilot brand tile (new JPG)
+  launch_tile: "/vercel.jpg",    // optional: launch triangle JPG
+  globe_tile: "/globe.jpg",      // optional: globe badge JPG
+  file_tile: "/file.jpg",        // optional: document JPG
+  window_tile: "/window.jpg",    // optional: window tile JPG
+
+  // existing icons (unchanged)
+  arrow_icon,
+  arrow_icon_dull,
+  menu_icon,
+  search_icon,
+  profile_icon,
+  copy_icon,
+  deepthink_icon,
+  chat_icon,
+  dislike_icon,
+  like_icon,
+  phone_icon,
+  phone_icon_dull,
+  pencil_icon,
+  delete_icon,
+  pin_icon,
+  regenerate_icon,
+  sidebar_icon,
+  sidebar_close_icon,
+  chat_icon_dull,
+  qrcode,
+  three_dots,
+  new_icon,
+};
